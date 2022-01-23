b00f1b9a3d9176eb66dc3fff2d0a136023d18e6d
ntaulya <netaaulya@gmail.com>
2022-01-23 17:13:04 +0700

    alhamdulillah page admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Admin;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('Admin.main');
+        $users = DB::table('users')->select('id','name','nomor_id', 'email', 'paket_program', 'ktp_kk')->get();
+
+        return view('Admin.main')->with('users', $users);
     }
 }
