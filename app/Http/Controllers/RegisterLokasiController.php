97ea41a07662503a01ecdbb52a544f46fde7b723
ntaulya <netaaulya@gmail.com>
2022-01-24 00:36:19 +0700

    tambah tugas, tambah materi admin, rapihin route

--- app/Http/Controllers/RegisterLokasiController.php
+++ app/Http/Controllers/RegisterLokasiController.php
@@ -23,4 +23,28 @@ class RegisterLokasiController extends Controller
         Auth::user()->dataUjians()->create($validation);
         return redirect(route('suksesDaftar'));
     }
+
+    public function index (Request $request) {
+        return view('lokasi.main');
+    }
+
+    public function taufiq (Request $request) {
+        return view('lokasi.taufiq');
+    }
+
+    public function taqwa (Request $request) {
+        return view('lokasi.taqwa');
+    }
+
+    public function RegLok1 (Request $request) {
+        return view('lokasi.RegLok1');
+    }
+
+    public function RegLok2 (Request $request) {
+        return view('lokasi.RegLok2');
+    }
+
+    public function suksesdaftar (Request $request) {
+        return view('lokasi.suksesdaftar');
+    }
 }
