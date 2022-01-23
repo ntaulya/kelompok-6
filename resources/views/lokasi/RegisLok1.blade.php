ecb510ffb108d41af6f5d20a3eca164dcd4e7485
itsnaufal <noriko.kazuko@gmail.com>
2022-01-23 14:22:58 +0700

    Update form regist lokasi ujian

--- resources/views/lokasi/RegisLok1.blade.php
+++ resources/views/lokasi/RegisLok1.blade.php
@@ -17,68 +17,71 @@
     <div class="card border border-primary mb-5" style="width: 100%;">
       <div class="card-body mb-5">
         <h5 class="card-title text-center text-primary mb-5 pb-2">DATA DIRI PESERTA UJIAN ONSITE PKBM AT-TAUFIQ</h5>
-        <form class="form-inline">
+        <form class="form-inline" action="{{ route('daftar-ujian') }}" method="POST">
+          @csrf
           <div class="form-group row">
             <label for="inputPassword" class="col-sm-2 col-form-label">Nomor Induk Siswa</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputPassword">
+              <input type="text" class="form-control" id="inputPassword" value="{{ Auth::user()->nomor_id }}" disabled>
             </div>
           </div>
 
           <div class="form-group row mt-3">
             <label for="inputPassword" class="col-sm-2 col-form-label">Nama Lengkap</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputPassword">
+              <input type="text" class="form-control" id="inputPassword" value="{{ Auth::user()->name }}" disabled>
             </div>
           </div>
 
           <div class="form-group row mt-3">
             <label for="inputPassword" class="col-sm-2 col-form-label">Paket Program</label>
             <div class="col-sm-10">
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
-                <label class="form-check-label" for="exampleRadios1">
-                  Paket Kesetaraan A
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                <label class="form-check-label" for="exampleRadios2">
-                  Paket Kesetaraan B
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
-                <label class="form-check-label" for="exampleRadios3">
-                  Paket Kesetaraan C
-                </label>
-              </div>
+              <input type="text" class="form-control" id="inputPassword" value="{{ Auth::user()->paket_program }}" disabled>              
             </div>
           </div>
 
           <div class="form-group row mt-3">
             <label for="inputPassword" class="col-sm-2 col-form-label">Mata Pelajaran</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputPassword">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="mata_pelajaran" id="mata_pelajaran1" value="IPA">
+                <label class="form-check-label" for="mata_pelajaran1">
+                  IPA
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="mata_pelajaran" id="mata_pelajaran2" value="IPS">
+                <label class="form-check-label" for="mata_pelajaran2">
+                  IPS
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="mata_pelajaran" id="mata_pelajaran3" value="MTK">
+                <label class="form-check-label" for="mata_pelajaran3">
+                  MTK
+                </label>
+              </div>
             </div>
           </div>
 
           <div class="form-group row mt-3">
             <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal Ujian</label>
             <div class="col-sm-10">
-              <input type="date" class="form-control" id="inputPassword">
+              <input type="date" class="form-control" id="inputPassword" name="tanggal_ujian">
             </div>
           </div>
 
           <div class="form-group row mt-3">
             <label for="inputPassword" class="col-sm-2 col-form-label">Waktu Pelaksanaan Ujian</label>
             <div class="col-sm-10">
-              <input type="time" class="form-control" id="inputPassword">
+              <input type="time" class="form-control" id="inputPassword" name="jam_ujian">
             </div>
           </div>
 
+          <input type="hidden" name="lokasi_ujian" value="PKBM AT-Taufiq">
+
           <div class="container text-center mt-5">
-            <a href="/suksesdaftar" class="btn btn-primary">Daftar Ujian</a></button>
+            <button type="submit" name="submit" class="btn btn-primary">Daftar Ujian</button>
           </div>
 
         </form>
