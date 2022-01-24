d3da9e5a14cd49cb5a367e0c979bd4f0fcdfde8b
ntaulya <netaaulya@gmail.com>
2022-01-25 01:56:59 +0700

    add function in admin controller, create materis migration, and change tambah materi blade

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,8 +15,26 @@ class AdminController extends Controller
         return view('Admin.main')->with('users', $users);
     }
 
-    public function tambahmateri() {
+    public function tambahmateri(Request $request) {
         return view('Admin.tambahmateri');
+    }
+
+    public function storetambahmateri(Request $request) {
+        $filename = $_FILES['materi']['name'];
+        $original_file_path = $_FILES['materi']['tmp_name'];
+        
+        Materi::create([
+            'mata_pelajaran' => $request->mata_pelajaran,
+            'jumlah_bab' => $request->jumlah_bab,
+            'jumlah_subbab' => $request->jumlah_subbab,
+            'materi' => $destination_file_path = $_SERVER['DOCUMENT_ROOT'].'/materi/'.$filename,
+        ]);
+
+        if(move_uploaded_file($original_file_path ,$destination_file_path)){
+            return redirect('/tambahmateri')->with('alert', 'Materi berhasil ditambah');
+        } else {
+            echo "Materi gagal ditambah";
+        }
     }
 
     public function tambahtugas() {
--- resources/views/Admin/tambahmateri.blade.php
+++ resources/views/Admin/tambahmateri.blade.php
@@ -5,25 +5,26 @@
 <div class="card border border-primary mb-5" style="width: 100%;">
     <div class="card-body mb-5">
     <h5 class="text-center">Tambah Materi</h5>
-      <form class="form-inline">
+    @csrf
+      <form class="form-inline" method="POST" action="{{}}">
         <div class="form-group row mt-5">
           <label for="inputPassword" class="col-sm-2 col-form-label">Nama Lengkap</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="nama_lengkap">
+            <input type="text" class="form-control" id="inputPassword" value="{{$user->$name}}" readonly name="nama_lengkap">
           </div>
         </div>
 
         <div class="form-group row mt-3">
           <label for="inputPassword" class="col-sm-2 col-form-label">Nomor ID Siswa</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="nomor_id">
+            <input type="text" class="form-control" id="inputPassword" value="{{$user->$nomor_id}}" readonly name="nomor_id">
           </div>
         </div>
 
         <div class="form-group row mt-3">
           <label for="inputPassword" class="col-sm-2 col-form-label">Paket Program</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="paket_program">
+            <input type="text" class="form-control" id="inputPassword" value="{{$user->$paket_program}}" readonly name="paket_program">
           </div>
         </div>
 
