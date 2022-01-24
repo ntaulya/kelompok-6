8afe70cb5fb11780a2b93e2b09e8a25231f46cf5
ntaulya <netaaulya@gmail.com>
2022-01-25 01:33:38 +0700

    page tambah materi admin, page tambah tugas admin

--- resources/views/Admin/tambahtugas.blade.php
+++ resources/views/Admin/tambahtugas.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.main')
-@section('title','Tambah Materi')
+@section('title','Tambah Tugas')
 @section('container')
 
 <div class="card border border-primary mb-5" style="width: 100%;">
     <div class="card-body mb-5">
-    <h5 class="text-center">Tambah Materi</h5>
+    <h5 class="text-center">Tambah Tugas</h5>
       <form class="form-inline">
         <div class="form-group row mt-5">
           <label for="inputPassword" class="col-sm-2 col-form-label">Nama Lengkap</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="nama_lengkap">
+            <input type="text" class="form-control" id="inputPassword" value="" readonly>
           </div>
         </div>
 
         <div class="form-group row mt-3">
           <label for="inputPassword" class="col-sm-2 col-form-label">Nomor ID Siswa</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="nomor_id">
+            <input type="text" class="form-control" id="inputPassword" value="" readonly>
           </div>
         </div>
 
         <div class="form-group row mt-3">
           <label for="inputPassword" class="col-sm-2 col-form-label">Paket Program</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword" value="" readonly name="paket_program">
+            <input type="text" class="form-control" id="inputPassword" value="" readonly>
           </div>
         </div>
 
@@ -42,32 +42,16 @@
         </div>
 
         <div class="form-group row mt-3">
-          <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah Bab</label>
-          <div class="col-sm-10">
-            <input type="number" class="form-control" id="inputPassword" name="jumlah_bab">
-          </div>
-        </div>
-
-        <div class="form-group row mt-3">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah Sub-Bab</label>
+            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Deskripsi Tugas</label>
             <div class="col-sm-10">
-              <input type="number" class="form-control" id="inputPassword" name="jumlah_subbab">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
             </div>
         </div>
 
-        <div class="form-group row mt-3">
-          <label for="inputPassword" class="col-sm-2 col-form-label">Upload Materi</label>
-          <div class="col-sm-10">
-            <input type="file" class="form-control-file" id="exampleFormControlFile1" name="materi">
-          </div>
-        </div>
-
         <div class="container text-center mt-5">
-          <button href="" class="btn btn-primary">Tambah Materi</button>
-          <button href="/Admin" class="btn btn-warning" style="margin-left: 10px">Batalkan</button>
-        </div>
-
-      </form>
+            <button href="" class="btn btn-primary">Tambah Tugas</button>
+            <button href="/Admin" class="btn btn-warning" style="margin-left: 10px">Batalkan</button>
+          </div>
     </div>
 </div>
 
